1d1098dca31fc96ba5f7cb1442b841c762b66ad2
Anguzu Daniel <anguzud7@gmail.com>
2024-07-24 22:28:07 +0300

    updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -103,8 +103,18 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  // Check if the book exists
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  // Get the reviews for the book
+  const reviews = books[isbn].reviews || {};
+
+  // Return the reviews
+  return res.status(200).json({ reviews });
 });
 
 module.exports.general = public_users;
