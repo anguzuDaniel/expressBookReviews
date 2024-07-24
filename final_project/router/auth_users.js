292a8f5321a4cea6c7a40c78ba5c1fb1591c200c
Anguzu Daniel <anguzud7@gmail.com>
2024-07-24 20:15:54 +0300

    delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -70,6 +70,30 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({ message: "Review added/updated successfully" });
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+  const username = req.user.username;
+
+  if (!books[isbn]) {
+    return res.status(400).json({ message: "Review content is required."})
+  }
+
+  if (!books[isbn].reviews || !books[isbn].reviews[username]) {
+    return res.status(404).json({ message: "Review not found" })
+  }
+
+  if (!books[isbn].reviews) {
+    books[isbn].reviews = {};
+  }
+
+  // Delete the review
+  delete books[isbn].reviews[username];
+
+  return res.status(200).json({ message: "Review deleted successfully" });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
